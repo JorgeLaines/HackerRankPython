a314c7ef3d98b50fa836a3258261768ca098bcf6
JorgeLaines <jorgelaines@gmail.com>
2023-05-31 09:56:54 -0500

    Viral Advertising

---
+++ ViralAdvertising.py
@@ -0,0 +1,29 @@
+# https://www.hackerrank.com/challenges/strange-advertising/problem
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'viralAdvertising' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER n as parameter.
+#
+
+def viralAdvertising(n):
+    # Write your code here
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    result = viralAdvertising(n)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
