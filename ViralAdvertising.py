268f38367adfe7817056e2be3e46d1484093ca57
JorgeLaines <jorgelaines@gmail.com>
2023-05-31 10:31:01 -0500

    Viral Advertising

--- ViralAdvertising.py
+++ ViralAdvertising.py
@@ -14,11 +14,21 @@ import sys
 # The function accepts INTEGER n as parameter.
 #
 
+
+def viralAdvertisingRecursive(n):
+    if(n > 1):
+        previousLiked, previousCumulative = viralAdvertisingRecursive(n - 1)
+        currentLiked = math.floor(previousLiked * 3 / 2)
+        return currentLiked, previousCumulative + currentLiked
+    else:
+        return 2, 2
+    
 def viralAdvertising(n):
-    # Write your code here
+    liked, cumulative = viralAdvertisingRecursive(n)
+    return cumulative
 
 if __name__ == '__main__':
-    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+    fptr = open('viralAdvertising.txt', 'w')
 
     n = int(input().strip())
 
